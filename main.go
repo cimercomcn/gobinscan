530d8c419d84e43ba3a8fa7fccf1fb3b4b1b2399
neumanlyu <neumannlyu@outlook.com>
2023-06-09 14:14:25 +0800

    fix: 检查是否加密时路径错误

--- main.go
+++ main.go
@@ -79,6 +79,7 @@ func Run() Result {
         oKLog.Logln("[提取完成] 提取的文件保存在 " + g_bin_extract_dir + " 目录下")
     }
 
+    g_bin_extract_dir = filepath.Join(g_bin_extract_dir, "_"+filepath.Base(g_bin_file_path)+".extracted")
     // 检查固件加密情况
     if isEncrypted() {
         defaultLog.Fatal("发现固件被加密，请解密后再尝试分析。\n")
@@ -94,7 +95,7 @@ func Run() Result {
 
     // 分析
     defaultLog.Info("\n\n\t\t\t[开始分析固件]\n\n")
-    analysis(filepath.Join(g_bin_extract_dir, "_"+filepath.Base(g_bin_file_path)+".extracted"))
+    analysis(g_bin_extract_dir)
 
     // 资源回收
     closePostgresDB()
